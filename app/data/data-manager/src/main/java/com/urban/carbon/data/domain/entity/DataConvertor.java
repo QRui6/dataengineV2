3cb2544589fcaaa62f39f838ad71c5bca19abdee
xgr <2004230016@email.cugb.edu.cn>
2025-07-22 14:41:21 +0800

    [Fix] 修改数据模块的配置与部分映射代码。

--- app/data/data-manager/src/main/java/com/urban/carbon/data/domain/entity/DataConvertor.java
+++ app/data/data-manager/src/main/java/com/urban/carbon/data/domain/entity/DataConvertor.java
@@ -2,14 +2,22 @@ package com.urban.carbon.data.domain.entity;
 
 import com.urban.carbon.api.data.manager.response.data.DataInfo;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface DataConvertor {
 
+    /**
+     * 数据源转换器
+     */
     DataConvertor INSTANCE = Mappers.getMapper(DataConvertor.class);
 
+    /**
+     * 数据源转换
+     *
+     * @param data 数据源
+     * @return 数据源信息
+     */
     DataInfo mapToVo(Data data);
 }
